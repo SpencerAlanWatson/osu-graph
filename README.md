0b418c1ef8a43e9f2bdcc994dff2fcde6d427022
Spencer Alan Watson <spenceralanwatson@gmail.com>
2016-08-19 11:20:02 -0500

    Modifying app to work with Heroku

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # osu-graph
+<<<<<<< HEAD
 A web application to compare osu!players top scores.
+=======
+A web application to compare osu!players top scores.
+>>>>>>> 43090a4a37ce56f2c844349be622fa192b56bf75
--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 var express = require("express");
 var app = express();
 
-app.use(express.static('public'));
+app.set('port', (process.env.PORT || 3000));
+app.use(express.static(__dirname + '/public'));
 
-
-
-app.listen(3000, function (){
-	console.log('We\'re live!  Running hot on port 3000!')
+app.listen(app.get('port'), function (){
+	console.log('We\'re live!  Running hot on port ' + app.get('port') + '!')
 });
